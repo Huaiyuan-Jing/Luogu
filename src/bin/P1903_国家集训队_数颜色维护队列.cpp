424f9fdc10905b305ab86b6baa48ad4f59285c2d
HYJing <ls.hyjing@gmail.com>
2024-08-20 15:46:06 -0500

    chore: Update .gitignore and .vscode/settings.json

---
+++ src/bin/P1903_国家集训队_数颜色维护队列.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define LL long long
+#define mp(a, b) make_pair(a, b)
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    cout << setprecision(20);
+    int n, m;
+    cin >> n >> m;
+    auto pens = vector<int>(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> pens[i];
+    }
+    
+}
