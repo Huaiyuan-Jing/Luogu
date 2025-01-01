1b0d0d9783d585292c283a9d97d3f9c986c3e055
Huaiyuan Jing <ls.hyjing@gmail.com>
2025-01-01 10:18:08 -0600

    * feat(P3376.rs): implement ISAP algorithm in MaxFlow struct * refactor(P3376.rs): replace dinic method with isap method in main function * refactor(P3376.rs): modify bfs and dfs methods in MaxFlow struct * feat(P3376.rs): add gap and n fields in MaxFlow struct

--- src/bin/P3376模板网络最大流.rs
+++ src/bin/P3376模板网络最大流.rs
@@ -6,26 +6,30 @@ struct Edge {
     flow: i32,
 }
 struct MaxFlow {
+    n: usize,
     s: usize,
     t: usize,
     head: Vec<usize>,
     edge: Vec<Edge>,
     maxflow: isize,
     depth: Vec<usize>,
+    gap: Vec<usize>,
 }
 impl MaxFlow {
     /// Creates a new `MaxFlow` instance with `n` nodes, source node `s`, and sink node `t`.
     pub fn new(n: usize, s: usize, t: usize) -> Self {
         Self {
+            n,
             s,
             t,
             head: vec![0x3f3f3f3f; n],
             edge: vec![],
             maxflow: 0,
-            depth: vec![0; n],
+            depth: vec![0x3f3f3f3f; n],
+            gap: vec![0; n * 2],
         }
     }
-    
+
     /// Adds a directed edge from `from` to `to` with capacity `cap` to the flow network.
     ///
     /// # Arguments
@@ -53,24 +57,26 @@ impl MaxFlow {
     /// Performs a BFS traversal of the residual graph to mark static nodes and find a shortest path from the source `s` to the sink `t`.
     ///
     /// Returns `true` if a path is found, and `false` otherwise.
-    fn bfs(&mut self) -> bool {
+    fn bfs(&mut self) {
         let mut q = std::collections::VecDeque::new();
-        q.push_back(self.s);
-        self.depth.fill(0x3f3f3f3f);
-        self.depth[self.s] = 0;
+        q.push_back(self.t);
+        self.depth[self.t] = 0;
+        self.gap[0] = 1;
         while !q.is_empty() {
             let u = q.pop_front().unwrap();
             let mut e = self.head[u];
             while e != 0x3f3f3f3f {
                 let v = self.edge[e].to;
-                if self.edge[e].flow < self.edge[e].cap && self.depth[v] > self.depth[u] + 1 {
-                    self.depth[v] = self.depth[u] + 1;
-                    q.push_back(v);
+                if self.depth[v] != 0x3f3f3f3f {
+                    e = self.edge[e].nxt;
+                    continue;
                 }
+                self.depth[v] = self.depth[u] + 1;
+                self.gap[self.depth[v]] += 1;
+                q.push_back(v);
                 e = self.edge[e].nxt;
             }
         }
-        self.depth[self.t] != 0x3f3f3f3f
     }
     /// Performs a DFS traversal of the residual graph to find a path from node `u` to the sink `t`.
     ///
@@ -82,44 +88,67 @@ impl MaxFlow {
     /// `flow`: The maximum flow to be pushed along the path.
     /// `edges`: The vector of edges in the residual graph.
     /// `cur`: The vector of current edges for each node in the residual graph.
-    fn dfs(&self, u: usize, flow: i32, edges: &mut Vec<Edge>, cur: &mut Vec<usize>) -> i32 {
+    fn dfs(
+        &self,
+        u: usize,
+        flow: i32,
+        edges: &mut Vec<Edge>,
+        cur: &mut Vec<usize>,
+        gap: &mut Vec<usize>,
+        depth: &mut Vec<usize>,
+    ) -> i32 {
         if u == self.t || flow == 0 {
             return flow;
         }
-        let mut res = 0;
+        let mut used = 0;
         let mut e = cur[u];
         while e != 0x3f3f3f3f {
             let v = edges[e].to;
-            if self.depth[v] == self.depth[u] + 1 {
+            if depth[v] == depth[u] - 1 {
                 let f = self.dfs(
                     v,
-                    std::cmp::min(flow - res, edges[e].cap - edges[e].flow),
+                    std::cmp::min(flow - used, edges[e].cap - edges[e].flow),
                     edges,
                     cur,
+                    gap,
+                    depth,
                 );
                 edges[e].flow += f;
                 edges[e ^ 1].flow -= f;
-                res += f;
-                if res == flow {
-                    break;
+                used += f;
+                if used == flow {
+                    return used;
                 }
             }
             e = self.edge[e].nxt;
             cur[u] = e;
         }
-        res
+        gap[depth[u]] -= 1;
+        if gap[depth[u]] == 0 {
+            depth[self.s] = self.n + 1;
+        }
+        depth[u] += 1;
+        gap[depth[u]] += 1;
+        used
     }
     /// Computes the maximum flow in the flow network using the Dinic's algorithm.
     ///
     /// This function repeatedly calls `bfs` to find a shortest augmenting path
     /// and `dfs` to augment the flow along the path. The maximum flow is
     /// accumulated in `maxflow` field.
-    pub fn dinic(&mut self) {
-        while self.bfs() {
+    pub fn isap(&mut self) {
+        self.bfs();
+        while self.depth[self.s] < self.n {
+            // println!("implement one dfs");
             let mut cur = self.head.clone();
             let mut edge = self.edge.clone();
-            self.maxflow += self.dfs(self.s, std::i32::MAX, &mut edge, &mut cur) as isize;
+            let mut gap = self.gap.clone();
+            let mut depth = self.depth.clone();
+            self.maxflow += self.dfs(self.s, std::i32::MAX, &mut edge, &mut cur, &mut gap, &mut depth) as isize;
             self.edge = edge;
+            self.gap = gap;
+            self.depth = depth;
+            // println!("gap: {:?}, depth: {:?}, maxflow: {}", self.gap, self.depth, self.maxflow);
         }
     }
 }
@@ -136,7 +165,7 @@ fn main() {
         };
         max_flow.add_edge(u as usize, v as usize, c);
     }
-    max_flow.dinic();
+    max_flow.isap();
     println!("{}", max_flow.maxflow);
 }
 fn read_numbers_from_input() -> Vec<i32> {
