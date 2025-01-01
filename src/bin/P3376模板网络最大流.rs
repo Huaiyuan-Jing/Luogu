9b19f3c6ffef84b37382047d98aaea3e8839bfff
Huaiyuan Jing <ls.hyjing@gmail.com>
2025-01-01 11:43:45 -0600

    * refactor(P3376.rs): encapsulate MaxFlow struct and related functions into maxflow module * refactor(P3376.rs): rename MaxFlow struct to Graph * refactor(P3376.rs): change function signatures to take Graph as argument * refactor(P3376.rs): simplify isap function by removing unnecessary clones

--- src/bin/P3376模板网络最大流.rs
+++ src/bin/P3376模板网络最大流.rs
@@ -1,35 +1,36 @@
-#[derive(Clone)]
-struct Edge {
-    to: usize,
-    nxt: usize,
-    cap: i32,
-    flow: i32,
-}
-struct MaxFlow {
-    n: usize,
-    s: usize,
-    t: usize,
-    head: Vec<usize>,
-    edge: Vec<Edge>,
-    maxflow: isize,
-    depth: Vec<usize>,
-    gap: Vec<usize>,
-}
-impl MaxFlow {
-    /// Creates a new `MaxFlow` instance with `n` nodes, source node `s`, and sink node `t`.
-    pub fn new(n: usize, s: usize, t: usize) -> Self {
-        Self {
-            n,
-            s,
-            t,
-            head: vec![0x3f3f3f3f; n],
-            edge: vec![],
-            maxflow: 0,
-            depth: vec![0x3f3f3f3f; n],
-            gap: vec![0; n * 2],
+mod maxflow {
+    #[derive(Clone)]
+    struct Edge {
+        to: usize,
+        nxt: usize,
+        cap: i32,
+        flow: i32,
+    }
+    pub struct Graph {
+        n: usize,
+        s: usize,
+        t: usize,
+        head: Vec<usize>,
+        edge: Vec<Edge>,
+        pub maxflow: isize,
+        depth: Vec<usize>,
+        gap: Vec<usize>,
+    }
+    impl Graph {
+        /// Creates a new `MaxFlow` instance with `n` nodes, source node `s`, and sink node `t`.
+        pub fn new(n: usize, s: usize, t: usize) -> Self {
+            Self {
+                n,
+                s,
+                t,
+                head: vec![0x3f3f3f3f; n],
+                edge: vec![],
+                maxflow: 0,
+                depth: vec![0x3f3f3f3f; n],
+                gap: vec![0; n * 2],
+            }
         }
     }
-
     /// Adds a directed edge from `from` to `to` with capacity `cap` to the flow network.
     ///
     /// # Arguments
@@ -37,44 +38,45 @@ impl MaxFlow {
     /// `from`: The node from which the edge originates.
     /// `to`: The node to which the edge points.
     /// `cap`: The capacity of the edge.
-    pub fn add_edge(&mut self, from: usize, to: usize, cap: i32) {
-        let m = self.edge.len();
-        self.edge.push(Edge {
+    pub fn add_edge(g: &mut Graph, from: usize, to: usize, cap: i32) {
+        let m = g.edge.len();
+        g.edge.push(Edge {
             to,
-            nxt: self.head[from],
+            nxt: g.head[from],
             cap,
             flow: 0,
         });
-        self.head[from] = m;
-        self.edge.push(Edge {
+        g.head[from] = m;
+        g.edge.push(Edge {
             to: from,
-            nxt: self.head[to],
+            nxt: g.head[to],
             cap: 0,
             flow: 0,
         });
-        self.head[to] = m + 1;
+        g.head[to] = m + 1;
     }
     /// Performs a BFS traversal of the residual graph to mark static nodes and find a shortest path from the source `s` to the sink `t`.
     ///
     /// Returns `true` if a path is found, and `false` otherwise.
-    fn bfs(&mut self) {
+    fn generate_depth(g: &mut Graph) {
+        g.depth = vec![0x3f3f3f3f; g.n];
         let mut q = std::collections::VecDeque::new();
-        q.push_back(self.t);
-        self.depth[self.t] = 0;
-        self.gap[0] = 1;
+        q.push_back(g.t);
+        g.depth[g.t] = 0;
+        g.gap[0] = 1;
         while !q.is_empty() {
             let u = q.pop_front().unwrap();
-            let mut e = self.head[u];
+            let mut e = g.head[u];
             while e != 0x3f3f3f3f {
-                let v = self.edge[e].to;
-                if self.depth[v] != 0x3f3f3f3f {
-                    e = self.edge[e].nxt;
+                let v = g.edge[e].to;
+                if g.depth[v] != 0x3f3f3f3f {
+                    e = g.edge[e].nxt;
                     continue;
                 }
-                self.depth[v] = self.depth[u] + 1;
-                self.gap[self.depth[v]] += 1;
+                g.depth[v] = g.depth[u] + 1;
+                g.gap[g.depth[v]] += 1;
                 q.push_back(v);
-                e = self.edge[e].nxt;
+                e = g.edge[e].nxt;
             }
         }
     }
@@ -88,47 +90,37 @@ impl MaxFlow {
     /// `flow`: The maximum flow to be pushed along the path.
     /// `edges`: The vector of edges in the residual graph.
     /// `cur`: The vector of current edges for each node in the residual graph.
-    fn dfs(
-        &self,
-        u: usize,
-        flow: i32,
-        edges: &mut Vec<Edge>,
-        cur: &mut Vec<usize>,
-        gap: &mut Vec<usize>,
-        depth: &mut Vec<usize>,
-    ) -> i32 {
-        if u == self.t || flow == 0 {
+    fn dfs(g: &mut Graph, u: usize, flow: i32, cur: &mut Vec<usize>) -> i32 {
+        if u == g.t || flow == 0 {
             return flow;
         }
         let mut used = 0;
         let mut e = cur[u];
         while e != 0x3f3f3f3f {
-            let v = edges[e].to;
-            if depth[v] == depth[u] - 1 {
-                let f = self.dfs(
+            let v = g.edge[e].to;
+            if g.depth[v] == g.depth[u] - 1 {
+                let f = dfs(
+                    g,
                     v,
-                    std::cmp::min(flow - used, edges[e].cap - edges[e].flow),
-                    edges,
+                    std::cmp::min(flow - used, g.edge[e].cap - g.edge[e].flow),
                     cur,
-                    gap,
-                    depth,
                 );
-                edges[e].flow += f;
-                edges[e ^ 1].flow -= f;
+                g.edge[e].flow += f;
+                g.edge[e ^ 1].flow -= f;
                 used += f;
                 if used == flow {
                     return used;
                 }
             }
-            e = self.edge[e].nxt;
+            e = g.edge[e].nxt;
             cur[u] = e;
         }
-        gap[depth[u]] -= 1;
-        if gap[depth[u]] == 0 {
-            depth[self.s] = self.n + 1;
+        g.gap[g.depth[u]] -= 1;
+        if g.gap[g.depth[u]] == 0 {
+            g.depth[g.s] = g.n + 1;
         }
-        depth[u] += 1;
-        gap[depth[u]] += 1;
+        g.depth[u] += 1;
+        g.gap[g.depth[u]] += 1;
         used
     }
     /// Computes the maximum flow in the flow network using the Dinic's algorithm.
@@ -136,19 +128,11 @@ impl MaxFlow {
     /// This function repeatedly calls `bfs` to find a shortest augmenting path
     /// and `dfs` to augment the flow along the path. The maximum flow is
     /// accumulated in `maxflow` field.
-    pub fn isap(&mut self) {
-        self.bfs();
-        while self.depth[self.s] < self.n {
-            // println!("implement one dfs");
-            let mut cur = self.head.clone();
-            let mut edge = self.edge.clone();
-            let mut gap = self.gap.clone();
-            let mut depth = self.depth.clone();
-            self.maxflow += self.dfs(self.s, std::i32::MAX, &mut edge, &mut cur, &mut gap, &mut depth) as isize;
-            self.edge = edge;
-            self.gap = gap;
-            self.depth = depth;
-            // println!("gap: {:?}, depth: {:?}, maxflow: {}", self.gap, self.depth, self.maxflow);
+    pub fn isap(g: &mut Graph) {
+        generate_depth(g);
+        while g.depth[g.s] < g.n {
+            let mut cur = g.head.clone();
+            g.maxflow += dfs(g, g.s, std::i32::MAX, &mut cur) as isize;
         }
     }
 }
@@ -157,16 +141,16 @@ fn main() {
         [n, m, s, t] => (*n, *m, *s, *t),
         _ => panic!("Extract numbers failed"),
     };
-    let mut max_flow = MaxFlow::new(n as usize + 1, s as usize, t as usize);
+    let mut g = maxflow::Graph::new(n as usize + 1, s as usize, t as usize);
     for _ in 0..m {
         let (u, v, c) = match read_numbers_from_input().as_slice() {
             [u, v, c] => (*u, *v, *c),
             _ => panic!("Extract numbers failed"),
         };
-        max_flow.add_edge(u as usize, v as usize, c);
+        maxflow::add_edge(&mut g,u as usize, v as usize, c);
     }
-    max_flow.isap();
-    println!("{}", max_flow.maxflow);
+    maxflow::isap(&mut g);
+    println!("{}", g.maxflow);
 }
 fn read_numbers_from_input() -> Vec<i32> {
     let mut input = String::new();
