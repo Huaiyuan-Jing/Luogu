62a2f226a917c523873ab2afaa0e84809af20e13
Huaiyuan Jing <ls.hyjing@gmail.com>
2025-01-27 15:37:35 -0600

    * feat(repetitivesong.rs): add logic to calculate minimum repetitions * feat(.cph/.repetitivesong.rs_d9849a12791af5c6d777083eae5074b9.prob): add new test cases for repetitivesong.rs

--- src/bin/repetitivesong.rs
+++ src/bin/repetitivesong.rs
@@ -1,10 +1,26 @@
+use std::collections::HashMap;
+
 fn main() {
-    let n = read_i32();
+    let n = read_i32() as usize;
     let mut arr: Vec<String> = vec![];
     for _ in 0..n {
         let mut s = String::new();
         std::io::stdin().read_line(&mut s).unwrap();
         arr.push(s.trim().to_string());
+    }
+    let mut previous_pos = HashMap::new();
+    let mut ans = i32::MAX;
+    for i in 0..n {
+        let s = &arr[i];
+        if let Some(pos) = previous_pos.get(s) {
+            ans = ans.min((i - *pos) as i32);
+        }
+        previous_pos.insert(s, i);
+    }
+    if ans == i32::MAX {
+        println!("0");
+    } else {
+        println!("{}", n as i32 - ans);
     }
 }
 fn read_i32() -> i32 {
