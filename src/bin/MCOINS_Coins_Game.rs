9d61f68d38a85c7c003c1220347f698c0a2aa598
Huaiyuan Jing <ls.hyjing@gmail.com>
2025-02-09 14:27:21 -0600

    * feat(MCOINS_Coins_Game.rs): add new test case and improve game logic * refactor(MCOINS_Coins_Game.rs): optimize read_i32 function for better input handling

--- src/bin/MCOINS_Coins_Game.rs
+++ src/bin/MCOINS_Coins_Game.rs
@@ -3,19 +3,29 @@ fn main() {
     let l = read_i32();
     let m = read_i32();
     let mut max_n = -i32::MAX;
-    let mut cases = vec![];
-    for _ in 0..m {
-        cases.push();
-    } 
+    let mut cases = vec![0; m as usize];
+    for i in 0..m as usize {
+        cases[i] = read_i32();
+        max_n = max_n.max(cases[cases.len() - 1]);
+    }
+    let opt = can_A_win(k, l, max_n);
+    for i in 0..m as usize {
+        if opt[cases[i] as usize] {
+            print!("A");
+        } else {
+            print!("B");
+        }
+    }
+    println!();
 }
-fn can_A_win(k: i32, l: i32, n: i32) -> vec![bool] {
+fn can_A_win(k: i32, l: i32, n: i32) -> Vec<bool> {
     let mut opt = vec![false; n as usize + 1];
-    for i in 1..=n {
-        if i - k >= 0 && !opt[i - k as usize] {
+    for i in 1..=n as usize {
+        if i as i32 - k >= 0 && !opt[i - k as usize] {
             opt[i as usize] = true;
             continue;
         }
-        if i - l >= 0 && !opt[i - l as usize] {
+        if i as i32 - l >= 0 && !opt[i - l as usize] {
             opt[i as usize] = true;
             continue;
         }
@@ -32,16 +42,20 @@ fn read_i32() -> i32 {
     io::stdout().flush().unwrap();
     let mut stdin = io::stdin();
     let mut buffer = [0; 1];
+    let mut started = false;
     while let Ok(()) = stdin.read_exact(buffer.as_mut()) {
         let c = buffer[0] as char;
-        if number.is_empty() && c == '-' {
-            number.push(c);
-            continue;
-        }
-        if c.is_numeric() {
-            number.push(c);
+        if !started {
+            if c == '-' || c.is_numeric() {
+                started = true;
+                number.push(c);
+            }
         } else {
-            break;
+            if c.is_numeric() {
+                number.push(c);
+            } else {
+                break;
+            }
         }
     }
     number.parse::<i32>().unwrap_or(0)
