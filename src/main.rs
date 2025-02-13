026250f5ee12a7e0ce26b393395d89401f8e78e0
Huaiyuan Jing <ls.hyjing@gmail.com>
2025-02-13 17:23:50 -0600

    * feat(main.rs): improve read_i32 function to handle leading non-numeric characters

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 #![allow(non_snake_case)]
 fn main() {
-    println!("Hello, world!");
+    println!("{}", read_i32());
 }
-
 fn read_i32() -> i32 {
     use std::io::{self, Read, Write};
     let mut number = String::new();
     io::stdout().flush().unwrap();
     let mut stdin = io::stdin();
     let mut buffer = [0; 1];
+    let mut started = false;
     while let Ok(()) = stdin.read_exact(buffer.as_mut()) {
         let c = buffer[0] as char;
-        if number.is_empty() && c == '-' {
-            number.push(c);
-            continue;
-        }
-        if c.is_numeric() {
-            number.push(c);
+        if !started {
+            if c == '-' || c.is_numeric() {
+                started = true;
+                number.push(c);
+            }
         } else {
-            break;
+            if c.is_numeric() {
+                number.push(c);
+            } else {
+                break;
+            }
         }
     }
     number.parse::<i32>().unwrap_or(0)
